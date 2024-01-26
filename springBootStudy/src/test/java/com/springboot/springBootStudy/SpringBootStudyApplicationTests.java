75cac1fbc3ce21334eca8277c5e56ab0599a80a4
cc7197 <cc7197@naver.com>
2024-01-26 22:49:43 +0900

    최초커밋

---
+++ springBootStudy/src/test/java/com/springboot/springBootStudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springBootStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
